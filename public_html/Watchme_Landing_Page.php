e42a3208c0f8da072e9c8be76f66bb18cdcc6dc4
Justin1307 <limkeathui_justin99@yahoo.com.sg>
2019-11-06 20:08:35 +0800

    header_main.php and footer_main.php created
    For session()

--- public_html/Watchme_Landing_Page.php
+++ public_html/Watchme_Landing_Page.php
@@ -23,44 +23,7 @@
 
 <body>
 <!-- Header-->
-<!--Start of Navigation Bar-->
-<div class="container-fluid">
-	<nav class="navbar navbar-expand-lg navbar-light navbar fixed-top bg-light">
-		  <div class="container-fluid">
-			<a class="navbar-brand" href="Watchme_Landing_Page.php"><img src="images/HomePage/Navbar icon.png" alt="Navbar Icon" title="Navbar Icon" width="150" height="80"/></a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		  </button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			  <ul class="navbar-nav ml-auto">
-				<li class="nav-item dropdown">
-				  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products</a>
-
-				  <div class="dropdown-menu dropdown-menu-left animate slideIn" aria-labelledby="navbarDropdown">
-					<a class="dropdown-item" href="SubPages/Product Overview/Casual Product Page.php">Casual Watches</a>
-					  <a class="dropdown-item" href="SubPages/Product Overview/Sports Product Page.php">Sports Watches</a>
-				  </div>
-				</li>
-				  <li class="nav-item">
-						<a href="SubPages/About Us/About Us.php" class="nav-link m-2 menu-item">About Us</a>
-					</li>
-
-					<li class="nav-item">
-						<a href="SubPages/Contact Us/Contact Us.php" class="nav-link m-2 menu-item">Contact Us</a>
-					</li>
-
-					<li class="nav-item">
-						<a href="SubPages/Membership Portal/Login Page.php" class="nav-link m-2 menu-item">Membership Portal</a>
-					</li>
-
-				  <li class="nav-item">
-						<a href="SubPages/Shopping Cart/Shopping Cart.php" class="nav-link m-2 menu-item">Shopping Cart</a>
-					</li>
-			  </ul>
-			</div>
-		  </div>
-	</nav>
-<!--End of Navigation Bar-->
+<?php include "header_main.php" ?>
 
 <!-- Header-->
 
@@ -164,60 +127,7 @@
 <!--End of Delivery Section-->
 
 <!--Start of footer-->
-<section class="footer">
-  <div class="container">
-    <div class="row text-center text-xs-center text-sm-left text-md-left">
-      <div class="col-xs-12 col-sm-4 col-md-4">
-        <h5>Category</h5> <!--Category-->
-        <ul class="list-unstyled links">
-          <li><a href="SubPages/Product Overview/Casual Product Page.php"><i class="fa fa-angle-double-right"></i>Casual Watches</a></li>
-          <li><a href="SubPages/Product Overview/Sports Product Page.php"><i class="fa fa-angle-double-right"></i>Sport Watches</a></li>
-        </ul>
-      </div>
-      <div class="col-xs-12 col-sm-4 col-md-4">
-        <h5>Company</h5><!--Company-->
-        <ul class="list-unstyled links">
-          <li><a href="SubPages/About Us/About Us.php"><i class="fa fa-angle-double-right"></i>About Us</a></li>
-          <li><a href="SubPages/Contact Us/Contact Us.php"><i class="fa fa-angle-double-right"></i>Contact Us</a></li>
-        </ul>
-      </div>
-      <div class="col-xs-12 col-sm-4 col-md-4">
-        <h5>NewsLetter</h5>
-        <ul class="list-unstyled links">
-                      <div id="mc_embed_signup"> <!-- NewsLetter -->
-                          <form target="_blank"
-                              action="javascript:void();"
-                              method="get" class="subscribe_form relative mail_part">
-                              <input type="email" name="email" id="newsletter-form-email" placeholder="Email Address"
-                                  class="placeholder hide-on-focus" onfocus="this.placeholder = ''"
-                                  onblur="this.placeholder = ' Email Address '">
-                              <button type="submit" name="submit" id="newsletter-submit"
-                                  class="email_icon newsletter-submit button-contactForm">Subscribe</button>
-                          </form>
-          </div>
-        </ul>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
-        <ul class="list-unstyled list-inline socialmedia text-center"> <!--Social Media Icons-->
-          <li class="list-inline-item"><a href="http://www.facebook.com" target="_blank"><i class="fa fa-facebook"></i></a></li>
-          <li class="list-inline-item"><a href="http://www.twitter.com" target="_blank"><i class="fa fa-twitter"></i></a></li>
-          <li class="list-inline-item"><a href="http://www.instagram.com" target="_blank"> <i class="fa fa-instagram"></i></a></li>
-        </ul>
-      </div>
-      <hr>
-    </div>
-    <div class="row"> <!--Credits & Copyright Section-->
-      <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-        <p><u><a href="../../Watchme_Landing_Page.php">Watch.me Pte Ltd Corporation</a></u></p>
-        <p class="h6">&copy All Rights Reversed.</p>
-        <p class="h6"><i class="fa fa-heart"></i> A huge thank you to <a href="https://shopino.com.ng/home" target="_blank"><u>Shopino</u></a> & <a href="https://www.watchshop.com" target="_blank"><u>Watch Shop</u></a> for the Website Design Inspiration & Watch Images </p>
-      </div>
-      <hr>
-    </div>
-  </div>
-</section>
+<?php include "footer_main.php" ?>
 <!--End of footer-->
 
 
