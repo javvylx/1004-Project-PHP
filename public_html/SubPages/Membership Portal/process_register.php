59b360f72cbe4f7d8ab9e25e3a53c62a50f4eff4
Javier Lim <javierlimzhenghao@hotmail.com>
2019-11-11 00:21:53 +0800

    Fixed broken links in process_register.php

--- public_html/SubPages/Membership Portal/process_register.php
+++ public_html/SubPages/Membership Portal/process_register.php
@@ -12,30 +12,30 @@ if (isset($_POST['signup-submit'])){
 	$cfmpassword = $_POST['cpwd'];
 	
 	if(check_empty($firstname) || check_empty($lastname) || check_empty($email) || check_empty($password) || check_empty($cfmpassword)) {
-		header("Location: Sign Up.php?error=emptyfields&fname=".$firstname."&lname=".$lastname."&email=".$email);
+		header("Location: Register.php?error=emptyfields&fname=".$firstname."&lname=".$lastname."&email=".$email);
 		exit();
 	}
 	
 	else if(!preg_match("/^[a-zA-Z]*$/", $firstname)) {
-		header("Location: Sign Up.php?error=invalidfirstname&lname=".$lastname."$&email=".$email);	
+		header("Location: Register.php?error=invalidfirstname&lname=".$lastname."$&email=".$email);	
 		$firstname = sanitize($_POST["firstname"]);
 		exit();
 	}
 	
 	else if(!preg_match("/^[a-zA-Z]*$/", $lastname)) {
-		header("Location: Sign Up.php?error=invalidlastname&fname=".$firstname."$&email=".$email);
+		header("Location: Register.php?error=invalidlastname&fname=".$firstname."$&email=".$email);
 		$lastname = sanitize($_POST["lastname"]);
 		exit();
 	}
 	
 	else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-		header("Location: Sign Up.php?error=invalidemail&fname".$firstname."&lname=".$lastname);
+		header("Location: Register.php?error=invalidemail&fname".$firstname."&lname=".$lastname);
 		$email = sanitize($_POST["email"]);
 		exit();
 	}
 	
 	else if($password !== $cfmpassword){
-		header("Location: Sign Up.php?error=passwordcheck&fname=".$firstname."&lname=".$lastname."$&email=".$email);
+		header("Location: Register.php?error=passwordcheck&fname=".$firstname."&lname=".$lastname."$&email=".$email);
 		$password = sanitize($_POST["password"]);
 		exit();
 	}
@@ -45,7 +45,7 @@ if (isset($_POST['signup-submit'])){
 		$stmt = mysqli_stmt_init($conn);
 		
 		if(!mysqli_stmt_prepare($stmt,$sql)){
-			header("Location: Sign Up.php?error=sqlerror");
+			header("Location: Register.php?error=sqlerror");
 			exit();
 		}
 		
@@ -55,7 +55,7 @@ if (isset($_POST['signup-submit'])){
 			mysqli_stmt_store_result($stmt);
 			$resultcheck = mysqli_stmt_num_rows($stmt);
 			if ($resultcheck > 0){
-				header("Location: Sign Up.php?error=emailtaken&email=".$email);
+				header("Location: Register.php?error=emailtaken&email=".$email);
 				exit();
 			}
 			else{
@@ -63,14 +63,14 @@ if (isset($_POST['signup-submit'])){
 			$sql = "INSERT INTO wm_users (fname, lname, email, password) VALUES (?, ?, ?, ?);";
 			$stmt = mysqli_stmt_init($conn);
 			if(!mysqli_stmt_prepare($stmt,$sql)){
-				header("Location: Sign Up.php?error=sqlinserror");
+				header("Location: Register.php?error=sqlinserror");
 				exit();
 			}
 			else {
 				$hashedpwd = password_hash($password, PASSWORD_DEFAULT);
 				mysqli_stmt_bind_param($stmt, "ssss", $firstname, $lastname,$email,$hashedpwd);
 				mysqli_stmt_execute($stmt);
-				header("Location: Sign Up.php?signup=success");
+				header("Location: Register.php?signup=success");
 				
 				exit();
 			}
@@ -81,7 +81,7 @@ if (isset($_POST['signup-submit'])){
 	mysqli_close($conn);
 }
 else{
-	header("Location: Sign Up.php");
+	header("Location: Register.php");
 	exit();
 }
 ?>
